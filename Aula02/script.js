1621ac875e50703352a9252423a5c052cc95d268
Daniel Steinbruch Pereira <108040161+danielsmartaccess@users.noreply.github.com>
2023-08-15 11:37:32 -0300

    ok

---
+++ Aula02/script.js
@@ -0,0 +1,18 @@
+function validar(){
+    var valor = document.getElementById("txtValor").value;
+    var pInfo = document.getElementById("info");
+    if (valor.length == 0 || isNaN(valor)) {
+        pInfo.innerHTML = "Valor inválido";
+        pInfo.style.background = "#ff0000";
+        return false;
+    } else if (valor <= 0 || valor > 10) {
+        pInfo.innerHTML = "Digite valores entre 1 e 10";
+        pInfo.style.color = "blue";
+        return false;
+    } else {
+        pInfo.innerHTML = "Valor válido";
+        pInfo.style.color = "green";
+        pInfo.setAttributeNS("background", "white", "color", "green");
+        return true;        
+    }
+}
