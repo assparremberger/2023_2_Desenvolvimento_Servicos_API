3f7ad4f63f6f3549b927f566cb73bc577021f647
ADALTO SELAU SPARREMBERGER <assparremberger@fspoa.br>
2023-08-22 11:13:27 -0300

    Aula02 - Objetos JS

--- Aula02/script.js
+++ Aula02/script.js
@@ -16,3 +16,28 @@ function validar(){
         return true;
     }
 }
+
+
+function calcular(){
+    valor01 = document.getElementById("txtValor01").value;
+    valor02 = document.getElementById("txtValor02").value;
+
+    if( valor01 != "" && valor02.length > 0 ){
+        v1 = parseFloat( valor01 );
+        v2 = parseFloat( valor02 );
+        var numero = { 
+            n1 : v1 ,
+            n2 : v2 ,
+            somar : function(){
+                return this.n1 + this.n2;
+            } ,
+            subtrair : function(){
+                return this.n1 - this.n2;
+            }
+        };
+
+        document.getElementById("pResult").innerHTML = 
+            "Soma: " + numero.somar() + 
+            "<BR>Subtração: " + numero.subtrair();
+    }
+}
